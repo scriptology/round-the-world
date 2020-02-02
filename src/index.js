cac19564ca03c735872a90873fc0c2a0b4d8c693
Andrey Solovyev <asolovyev@condenast.ru>
2020-02-02 03:54:35 +0300

    add new map

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import CanvasMap from './canvas-map'
 
 let map=CanvasMap({
   textContainer:document.querySelector('.text'),
-  mapSrc:'img/map.svg',
+  mapSrc:'img/world_map_blank.svg',
   trailVisitedColor:'#47DBB4',
   fontPresentColor:'#5D5C56',
 }).appendTo('.container')
